173f6b21e730071635279511584ca6d05434e2c6
saliouvj <m.saliou90@gmail.com>
2021-01-10 22:36:48 +0100

    fin exo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # js_variable_exercice2
-# js_variable_exercice2
---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+// let phrase = "j'aime les tartines et j'en mange"
+// console.log(phrase)
+// let phrase2 = "4 par jours"
+
+// let result = phrase + ' ' + phrase2
+// console.log(result)
+
+// let nombre = 5
+// let nombre2 = 35
+// console.log(nombre + nombre2)
+// console.log(phrase + nombre)
+
+let salutation = "salut tout le monde"
+console.log(salutation)
+let bye = "aurevoir"
+console.log(bye)
+
+salutation = bye
+console.log(salutation)
+
+let number1 = 4
+let number2 = "6"
+
+console.log(number1 > number2)
+console.log(number < number2)
+console.log(number == number2) //égal
+console.log(number1 != number2) //diff
+console.log(number1 === number2) //strict égal
+console.log(number1 !==) //strict diff
